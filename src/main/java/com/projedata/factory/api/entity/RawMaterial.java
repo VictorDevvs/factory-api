55682de80c51a996a910a3b0be8a67fab9f44a73
VictorDevvs <victorsantosdevvs@gmail.com>
2026-02-23 18:58:58 -0300

    criando a entidade raw material

---
+++ src/main/java/com/projedata/factory/api/entity/RawMaterial.java
@@ -0,0 +1,34 @@
+package com.projedata.factory.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "raw_materials")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RawMaterial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal stockQuantity;
+
+    @Column(nullable = false)
+    private String unit;
+
+}
