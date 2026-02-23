ae75efdc495419af973e033f411339113e5d44a4
VictorDevvs <victorsantosdevvs@gmail.com>
2026-02-23 19:10:17 -0300

    criando as entidades product e product compositions e seus relacionamentos

---
+++ src/main/java/com/projedata/factory/api/entity/Product.java
@@ -0,0 +1,36 @@
+package com.projedata.factory.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal saleValue;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductComposition> compositions = new ArrayList<>();
+}
---
+++ src/main/java/com/projedata/factory/api/entity/ProductComposition.java
@@ -0,0 +1,33 @@
+package com.projedata.factory.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product_compositions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductComposition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "raw_material_id", nullable = false)
+    private RawMaterial rawMaterial;
+
+    @Column(nullable = false)
+    private BigDecimal requiredQuantity;
+}
